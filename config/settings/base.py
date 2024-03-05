4668942a503c14181dd21af0f2b3de41e007fc0c
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2024-03-05 22:33:26 +0500

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -222,7 +222,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Javohir Elmurodov""", "elmurodovjavohir@gmail.com")]
+ADMINS = [("""John Doe""", "john.doe@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
